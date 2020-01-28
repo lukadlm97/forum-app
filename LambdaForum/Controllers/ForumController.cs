08cc9190f71ef6bc126a9c8100ff6685f3dd4bc5
Luka Radovanovic <lukadlm97@gmail.com>
2020-01-28 19:12:36 +0100

    Created view-model for view forum

---
+++ LambdaForum/Controllers/ForumController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LambdaForum.Data;
+using LambdaForum.Models.Forum;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LambdaForum.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+        public IActionResult Index()
+        {
+            var forums = _forumService.GetAll()
+                .Select(f => new ForumListingModel { 
+                    Id = f.Id,
+                    Name = f.Title,
+                    Description = f.Description
+                });
+
+            var model = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+
+            return View(model);
+        }
+    }
+}
