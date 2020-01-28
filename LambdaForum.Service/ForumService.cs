62f78aa9ac05ad721f296e9c35ec917ae4097161
Luka Radovanovic <lukadlm97@gmail.com>
2020-01-28 18:57:17 +0100

    Created Service Layer

---
+++ LambdaForum.Service/ForumService.cs
@@ -0,0 +1,55 @@
+﻿using LambdaForum.Data;
+using LambdaForum.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LambdaForum.Service
+{
+    public class ForumService : IForum
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ForumService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task Create(Forum forum)
+        {
+            _context.AddAsync(forum);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            return _context.Forums
+                .Include(f => f.Posts);
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
