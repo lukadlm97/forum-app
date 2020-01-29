4c633ba83b37aae247d3b8321f00612ab0524065
Luka Radovanovic <lukadlm97@gmail.com>
2020-01-29 01:19:21 +0100

    Created but not implemented PostController

---
+++ LambdaForum/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LambdaForum.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LambdaForum.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPost _postsService;
+
+        public PostController(IPost postsService)
+        {
+            _postsService = postsService;
+        }
+        public IActionResult Index(int id)
+        {
+            var post = _postsService.GetById(id);
+
+            return View();
+        }
+    }
+}
