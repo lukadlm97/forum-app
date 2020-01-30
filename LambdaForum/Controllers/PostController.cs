7bc25c89e50b8c450841ebd571c2623e939ce186
Luka Radovanovic <lukadlm97@gmail.com>
2020-01-30 16:16:06 +0100

    Created methodn on controller for creating new post

--- LambdaForum/Controllers/PostController.cs
+++ LambdaForum/Controllers/PostController.cs
@@ -6,6 +6,7 @@ using LambdaForum.Data;
 using LambdaForum.Data.Models;
 using LambdaForum.Models.Post;
 using LambdaForum.Models.Reply;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LambdaForum.Controllers
@@ -13,10 +14,14 @@ namespace LambdaForum.Controllers
     public class PostController : Controller
     {
         private readonly IPost _postsService;
+        private readonly IForum _forumService;
+        private static UserManager<ApplicationUser> _userManager;
 
-        public PostController(IPost postsService)
+        public PostController(IPost postsService,IForum forumService,UserManager<ApplicationUser> userManager)
         {
             _postsService = postsService;
+            _forumService = forumService;
+            _userManager = userManager;
         }
         public IActionResult Index(int id)
         {
@@ -51,6 +56,44 @@ namespace LambdaForum.Controllers
                 Created = reply.Created,
                 ReplyContent = reply.Content
             });
+        }
+
+
+        public IActionResult Create(int id)
+        {
+            var forum = _forumService.GetById(id);
+
+            var model = new NewPostModel
+            {
+                ForumName = forum.Title,
+                ForumId = forum.Id,
+                ForumImageUrl = forum.ImageUrl,
+                AuthorName = User.Identity.Name
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost(NewPostModel model)
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = await _userManager.FindByIdAsync(userId);
+            var post = BuildPost(model, user);
+
+            await _postsService.Add(post);
+
+            return RedirectToAction("Index", "Post",post.Id);
+        }
+
+        private Post BuildPost(NewPostModel model, ApplicationUser user)
+        {
+            return new Post
+            {
+                Title = model.Title,
+                Content = model.Content,
+                Created = DateTime.Now,
+                User = user
+            };
         }
     }
 }
